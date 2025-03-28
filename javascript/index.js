b1cea0c2d26e7912b1770c9d0bb9ad8091503a0f
tmuziringa <tmuziringa@gmail.com>
2025-03-28 01:05:22 +0000

    added link to js

--- javascript/index.js
+++ javascript/index.js
@@ -36,8 +36,8 @@ function updateCity(event){
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>`
-
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+<a href="/">All cities</a>`
 }
 
 updateTime();
