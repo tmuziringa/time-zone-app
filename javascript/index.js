7e9d369fb3a2ea7df2b5b7510d1038a7700cec32
tmuziringa <tmuziringa@gmail.com>
2025-03-28 01:15:43 +0000

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -37,7 +37,8 @@ function updateCity(event){
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-</div><a href="/">All cities</a>`;
+</div>
+<div><a href="/">All cities</a></div>`;
 }
 
 updateTime();
